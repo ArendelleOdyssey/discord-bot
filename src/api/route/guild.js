1f6bc6916f3dd70705719db380b315f85c95acae
Matthieu Dehon <89matt89.md@gmail.com>
2021-01-20 21:32:35 +0100

    add banner API

--- src/api/route/guild.js
+++ src/api/route/guild.js
@@ -1,11 +1,22 @@
 const path = require('path');
 const createError = require('http-errors');
+const { format } = require('path');
 var scriptName = path.basename(__filename).replace('.js', '');
 
 module.exports = function(app, client, config, sql, guild){
     app.get('/'+scriptName, async (req, res, next) => {
         try{
             res.json(await client.guilds.fetch(guild))
+        } catch (err){
+            console.error(err)
+            next(createError(500))
+        }
+    })
+    app.get('/'+scriptName+'/banner', async (req, res, next) => {
+        try{
+            var bannerURL = await client.guilds.fetch(guild).bannerURL({format: 'png', size: 4096, dynamic: true})
+            if (bannerURL == null) next(createError(404))
+            else res.sendFile(bannerURL)
         } catch (err){
             console.error(err)
             next(createError(500))
