3484bf06be433fcdd637f5eb6db499ba266d5b7b
Matthieu Dehon <89matt89.md@gmail.com>
2021-03-16 18:45:46 +0100

    Move files to useful folders

--- src/api/route/noToken/banner.js
+++ src/api/route/noToken/banner.js
@@ -4,15 +4,7 @@ const download = require('download')
 var scriptName = path.basename(__filename).replace('.js', '');
 
 module.exports = function(app, client, config, sql, guild){
-    app.get('/'+scriptName, async (req, res, next) => {
-        try{
-            res.json(await client.guilds.fetch(guild))
-        } catch (err){
-            console.error(err)
-            next(createError(500))
-        }
-    })
-    app.get('/'+scriptName+'/banner', async (req, res, next) => {
+    app.get('/guild/banner', async (req, res, next) => {
         try{
             var fetchGuild = await client.guilds.fetch(guild)
             var bannerURL = fetchGuild.bannerURL({format: 'png', size: 4096, dynamic: true})
