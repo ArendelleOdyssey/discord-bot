a5d9df015587b0b3820fbb1fa0157c3cedb25898
Matthieu Dehon <89matt89.md@gmail.com>
2021-03-18 12:53:21 +0100

    Update jinxs.js

--- src/cmds/ao/jinxs.js
+++ src/cmds/ao/jinxs.js
@@ -5,6 +5,12 @@ function randomItem(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
 
+function imageProbality(){
+    var num = Math.random();
+    if (num < 0.95) return false;
+    else return true
+  }
+
 module.exports = function(message, client, prefix, sql){
     if (message.content.toLowerCase() == prefix + 'jinxs'){
         sql.query("SELECT * FROM `jinxs`", (err,res)=>{
@@ -14,12 +20,16 @@ module.exports = function(message, client, prefix, sql){
                 client.users.cache.find(u => u.id == config.discord.owner_id).send(`:warning: Error getting jinxs counter: \`\`\`${err}\`\`\``)
             } else {
                 const randommsgs = JSON.parse(fs.readFileSync('./data/jinx_counter_messages.json'))
+                const images = [
+                    'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/spiderman-meme-1545383498.jpg'
+                ]
                 let embed = new Discord.MessageEmbed()
                 embed.setTitle('Jinx! counter')
                 .setDescription(`We are at ${res[0].count} Jinxs!, ${randomItem(randommsgs)}`)
                 .setThumbnail('https://cdn.discordapp.com/emojis/755280811447812127.png?v=1')
                 .setFooter('The latest jinx was sent:')
                 .setTimestamp(res[0].last)
+                if (imageProbality()) embed.setImage(randomItem(images))
                 message.channel.send(embed)
             }
         })
