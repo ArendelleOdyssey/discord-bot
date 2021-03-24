9b2750b2b0502f20b5e4facf5013cad1b6491fee
Matthieu Dehon <89matt89.md@gmail.com>
2021-03-24 21:05:30 +0100

    Add shuffle condition on skip

--- src/cmds/music/skip.js
+++ src/cmds/music/skip.js
@@ -4,7 +4,12 @@ const ytdl = require('ytdl-core');
 function skip(message, client, serverQueue, play, queue) {
 	if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to stop the music!');
 	if (!serverQueue) return message.channel.send('There is no song that I could skip!');
-    serverQueue.songs.shift();
+    if (serverQueue.shuffle == true) {
+        let random = Math.floor(Math.random() * serverQueue.songs.length)
+        var newFirst = serverQueue.songs[random]
+        serverQueue.songs.splice(newFirst, 1);
+        serverQueue.songs.unshift(newFirst)
+    } else serverQueue.songs.shift();
     if (serverQueue.songs.length < 1){
         serverQueue.connection.disconnect();
         queue.delete(message.guild.id)
