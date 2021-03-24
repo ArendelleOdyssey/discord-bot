77d76b159f1c6f4ebaa6c90a65e5489e38d7e523
Matthieu Dehon <89matt89.md@gmail.com>
2021-03-24 16:42:18 +0100

    Update skip.js

--- src/cmds/music/skip.js
+++ src/cmds/music/skip.js
@@ -1,11 +1,12 @@
 const Discord = require('discord.js')
-const ytdl = require('ytdl-core')
+const ytdl = require('ytdl-core');
 
-function skip(message, serverQueue) {
+function skip(message, serverQueue, play, queue) {
 	if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to stop the music!');
 	if (!serverQueue) return message.channel.send('There is no song that I could skip!');
     serverQueue.songs.shift();
     serverQueue.connection.dispatcher.end();
+    play(message.guild, serverQueue.songs[0], queue)
     message.react('⏯')
 }
 
