153160dd4c689cb14e60776cb7fb07bbb1ec3484
Matthieu Dehon <89matt89.md@gmail.com>
2021-03-24 16:54:36 +0100

    Add stop in skip

--- src/cmds/music/skip.js
+++ src/cmds/music/skip.js
@@ -5,9 +5,15 @@ function skip(message, serverQueue, play, queue) {
 	if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to stop the music!');
 	if (!serverQueue) return message.channel.send('There is no song that I could skip!');
     serverQueue.songs.shift();
-    serverQueue.connection.dispatcher.end();
-    play(message.guild, serverQueue.songs[0], queue)
-    message.react('⏯')
+    if (serverQueue.songs.length < 1){
+        serverQueue.connection.disconnect();
+        queue.delete(message.guild.id)
+        message.react('👋')
+    } else {
+        serverQueue.connection.dispatcher.end();
+        play(message.guild, serverQueue.songs[0], queue)
+        message.react('⏯')
+    }
 }
 
 module.exports = skip
