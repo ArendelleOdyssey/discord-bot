bf6cb9713190a3fabcc13bf237bb54e8f675df3d
Matthieu Dehon <89matt89.md@gmail.com>
2021-03-24 17:48:41 +0100

    fix search and playlist

--- src/cmds/music/execute.js
+++ src/cmds/music/execute.js
@@ -6,74 +6,17 @@ const wait = require('util').promisify(setTimeout);
 
 async function playlist(message, args, play, queue, serverQueue){
 	try{
-        ytpl(args[0].replace('https://www.youtube.com/playlist?list=',''), async function(err, playlist) {
-		if(err){
-			console.error(err)
-			message.channel.send('Error: ' + err)
-		}
-
-		const voiceChannel = message.member.voice.channel;
-		if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!').then(m=>message.channel.stopTyping(true))
-		const permissions = voiceChannel.permissionsFor(message.client.user);
-		if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
-			return message.channel.send('I need the permissions to join and speak in your voice channel!').then(m=>message.channel.stopTyping(true))
-		}
-
-		message.channel.send(`Adding ${playlist.items.length} songs to the queue, please be patient (it takes some times)`)
-
-		if (!serverQueue) {
-			const queueContruct = {
-				textChannel: message.channel,
-				voiceChannel: voiceChannel,
-				connection: null,
-				songs: [],
-				volume: 100,
-				playing: true,
-				loop : false,
-				shuffle : false,
-			};
-  
-			queue.set(message.guild.id, queueContruct);
-
-			await Promise.all(playlist.items.map(async (item) => {
-				const songInfo = await ytdl.getInfo(item.url_simple);
-				const song = {
-					title: songInfo.title,
-					url: songInfo.video_url,
-				};
-				queueContruct.songs.push(song)
-				console.log(`${songInfo.title} (${songInfo.video_url}) added in ${message.guild.name}`)
-			  }));
-
-				try {
-					var connection = await voiceChannel.join();
-					queueContruct.connection = connection;
-					await play(message.guild, queueContruct.songs[0], queue);
-					message.channel.stopTyping(true)
-					message.react('▶')
-				} catch (err) {
-					console.error(err);
-					queue.delete(message.guild.id);
-					return message.channel.send(err);
-				}
-			
-		 } else {
-			await Promise.all(playlist.items.map(async (item) => {
-				const songInfo = await ytdl.getInfo(item.url_simple);
-				const song = {
-					title: songInfo.videoDetails.title,
-					url: songInfo.videoDetails.video_url,
-				};
-				serverQueue.songs.push(song)
-				console.log(`[Playlist] ${song.title} (${song.url}) added in ${message.guild.name}`)
-			  }));
-			 message.channel.stopTyping(true)
-		}
-	});
-        } catch (err) {
-                console.error(err)
-                message.channel.send('Error: ' + err)
-        }
+        var playlist = await ytpl(args[0].replace('https://www.youtube.com/playlist?list=',''))
+		
+        playlist.items.forEach(item=>{
+            var url = item.shortUrl
+            launch(message, url, play, queue, serverQueue)
+        })
+		
+    } catch (err) {
+        console.error(err)
+        message.channel.send('Error: ' + err)
+    }
 }
 
 async function launch(message, url, play, queue, serverQueue){
@@ -127,16 +70,19 @@ async function launch(message, url, play, queue, serverQueue){
         }
 }
 
-function search(message, args, play, serverQueue, queue){
+async function search(message, args, play, serverQueue, queue){
 	try{
-		ytsr(args.join(' '), {limit: 1}, async function(err, searchResults) {
-			if(err){
-				console.error(err)
-				message.channel.send(err)
-			}
-			var url = searchResults.items[0].link
-			launch(message, url, play, queue, serverQueue)
-	    });
+		let filter;
+	    const filters1 = await ytsr.getFilters(args.join(' '))
+		const filter1 = filters1.get('Type').find(o => o.name === 'Video');
+		const filters2 = await ytsr.getFilters(filter1.url)
+        const filter2 = filters2.get('Duration').find(o => o.name.startsWith('Short'));
+        var options = {
+            limit: 1
+        }
+        var searchResults = ytsr(filter2.url, options)
+        var url = searchResults.items[0].url
+        launch(message, url, play, queue, serverQueue)
 	} catch(err){
 		console.error(err)
 		message.channel.send(err)
