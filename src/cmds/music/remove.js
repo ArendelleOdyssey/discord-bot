68fb26adf1b00cea64f0895660317a98512475f8
Matthieu Dehon <89matt89.md@gmail.com>
2021-03-24 19:29:38 +0100

    Update remove.js

--- src/cmds/music/remove.js
+++ src/cmds/music/remove.js
@@ -5,22 +5,25 @@ function remove(message, serverQueue) {
 	let args = message.content.split(' ');
     args.shift();
 
-    if (args.length < 1) return message.channel.send('Enter a number in the queue to be removed')
+    if (args.length < 1) return message.channel.send('Enter a number in the queue to be removed, or put `all` to remove all the waiting songs')
     
-    var argNumber = Number(args[0])
+    if (args[0].toLowerCase() == 'all'){
+        serverQueue.songs.splice(1, serverQueue.songs.length)
+        return message.channel.send('Removed all waiting queue')
+    } else {
+        var argNumber = Number(args[0])
 
-    if (isNaN(argNumber)) {
-        if (args[0].toLowerCase() == 'all'){
-            serverQueue.songs.splice(1, serverQueue.songs.length)
-            return message.channel.send('Removed all waiting queue')
-        } else return message.react('❌')   
-    }
+        if (isNaN(argNumber)) {
+            if (args[0].toLowerCase() == 'all'){
+                serverQueue.songs.splice(1, serverQueue.songs.length)
+                return message.channel.send('Removed all waiting queue')
+            } else return message.react('❌')   
+        }
 
-    if (argNumber > -1){
         var song = serverQueue.songs[argNumber]
         serverQueue.songs.splice(argNumber, 1)
         return message.channel.send('Removed ' + song.title)
-    } else return message.react('❌')
+    }
     
 }
 
