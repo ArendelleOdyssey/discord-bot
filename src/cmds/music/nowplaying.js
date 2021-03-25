ac38731d181819232ef6a5bfdda0bc64a1c66993
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-03-25 21:52:15 +0100

    Add a text when it's in loop and/or shuffle is enabled

--- src/cmds/music/nowplaying.js
+++ src/cmds/music/nowplaying.js
@@ -6,8 +6,10 @@ function nowplaying(message, client, serverQueue) {
     let embed = new Discord.MessageEmbed()
     embed.setColor('RANDOM')
     embed.setTitle('Now Playing:')
-    embed.setDescription(`[${serverQueue.songs[0].title}](${serverQueue.songs[0].url})`)
-    if (serverQueue.songs[1]) embed.addField('Next song:', `[${serverQueue.songs[1].title}](${serverQueue.songs[1].url})`)
+    embed.setDescription(`[${serverQueue.songs[0].title}](${serverQueue.songs[0].url})${serverQueue.loop == true ? ' 🔂': serverQueue.loop == "all" ? ' 🔁' : ''}${serverQueue.shuffle && serverQueue.songs[1] ? '\n\n🔀 Shuffle is enabled, next song is a random song from the queue.': ''}`)
+    if (serverQueue.shuffle == false) {
+        if (serverQueue.songs[1]) embed.addField('Next song:', `[${serverQueue.songs[1].title}](${serverQueue.songs[1].url})`)
+    }
     // embed.setFooter(`${client.user.tag}, now serving music in ${message.guild.name}`, client.user.displayAvatarURL)
     message.channel.send(embed)
 }
