c2a6b8081c762d5b4b243935d719545807c2cd4f
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-03-25 23:22:38 +0100

    fix bug

--- src/cmds/music/_music-index.js
+++ src/cmds/music/_music-index.js
@@ -11,7 +11,7 @@ module.exports = function(message, client, prefix, sql){
     else if (message.content.toLowerCase().startsWith(`${prefix}play`) || message.content.toLowerCase().startsWith(`${prefix}p`)) require('./execute.js')(message, client, play, serverQueue, queue, sql);
     else if (message.content.toLowerCase().startsWith(`${prefix}remove`)) require('./remove.js')(message, serverQueue);
     else if (message.content.toLowerCase() == `${prefix}skip` || message.content.toLowerCase() == `${prefix}s`) require('./skip.js')(message, client, serverQueue, play, queue, sql);
-    else if (message.content.toLowerCase() == `${prefix}stop` || message.content.toLowerCase() == `${prefix}leave`) require('./stop.js')(message, serverQueue, queue);
+    else if (message.content.toLowerCase() == `${prefix}stop` || message.content.toLowerCase() == `${prefix}leave`) require('./stop.js')(message, serverQueue, queue, sql);
     else if (message.content.toLowerCase().startsWith(`${prefix}search`)) require('./search.js')(message, prefix);
     else if (message.content.toLowerCase() == `${prefix}queue` || message.content.toLowerCase() == `${prefix}q`) require('./queue.js')(message, serverQueue);
 	else if (message.content.toLowerCase() == `${prefix}nowplaying` || message.content.toLowerCase() == `${prefix}np`) require('./nowplaying.js')(message, client, serverQueue);
