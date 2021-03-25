9358ea24a8e7369fc734db20625c1e35456dd84b
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-03-25 23:20:01 +0100

    Update skip.js

--- src/cmds/music/skip.js
+++ src/cmds/music/skip.js
@@ -4,6 +4,12 @@ const ytdl = require('ytdl-core');
 function skip(message, client, serverQueue, play, queue, sql) {
 	if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to stop the music!');
 	if (!serverQueue) return message.channel.send('There is no song that I could skip!');
+    sql.query("UPDATE `music` SET `isPlaying` = 0 WHERE `link` = ? AND `isPlaying` = 1;", serverQueue.songs[0].url, (err)=>{
+        if (err){
+            console.error(err)
+            client.users.cache.find(u => u.id == config.discord.owner_id).send(`:warning: Error updating music log: \`\`\`${err}\`\`\``)
+        }
+    })
     if (serverQueue.shuffle == true) {
         let random = Math.floor(Math.random() * serverQueue.songs.length)
         var newFirst = serverQueue.songs[random]
